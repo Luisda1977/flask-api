3e349572ba11f8aebd922217a3fd5fb4bdaf1da1
Luisda <lsanchezroldan@gmail.com>
2022-07-14 21:30:06 +0200

    configuración y recogemos models y estáticos de proyecto anterior

--- balance/views.py
+++ balance/views.py
@@ -1,6 +1,9 @@
+from config import RUTA
 from . import app
 
 
 @app.route("/")
 def inicio():
-    return "Vamos a por la API"
+    return (f"La ruta del archivo de datos es: "
+            f"{app.config['RUTA']}<br>Secret Key "
+            f"{app.config['SECRET_KEY']}")
